c32fbc169c5e89b085efeb2f4b78a858cbd89c85
Thunyavat Thanakornsombat <thunyavat.comEngr@hotmail.com>
2024-04-29 10:14:51 +0700

    Update find_event function but still broken with find()

--- backend/event/models/event.py
+++ backend/event/models/event.py
@@ -67,7 +67,7 @@ def create_event(event_info, image):
     event["durationDays"] = 1 + ((event["eventEnd"] - event["eventStart"]).days)
     db = get_db_handle("myEvent", "localhost", "27017", "root", "password")
     table = db["events"]
-    # table.create_index([("location", GEOSPHERE)], unique=False)
+    table.create_index([("location", GEOSPHERE)], unique=False)
     table.insert_one(event)
     return event["eventId"]
 
@@ -97,24 +97,37 @@ def create_event(event_info, image):
 
 def find_event(longlat: list, event_type="", age_min=0, age_max=0, title=""):
     try:
-        db = get_db_handle("myEvent", "localhost", "27017", "root", "password")
-        table = db["events"]
         max_distance_radians = 10000 / 6371000
         query = {"location": SON([("$nearSphere", longlat), ("$maxDistance", max_distance_radians)])}
-        print(query)
         if event_type:  # แก้ไขการตรวจสอบประเภทกิจกรรม
+            print("type")
             query["type"] = event_type
         if age_min > 0:
+            print("ageMin")
             query["ageMin"] = {"$gte": age_min}
         if age_max > 0:
+            print("ageMax")
             query["ageMax"] = {"$lte": age_max}
-        data = list(table.find(query))
-        
+        db = get_db_handle("myEvent", "localhost", "27017", "root", "password")
+        table = db["events"]
+
+        # Problem with this fricking fuction
+        cursor = table.find(query, {'_id': False})
+        print(cursor)
+        if cursor.retrieved == 0:
+            return []
+        data = list(cursor) 
+        cursor.close()
+
         for d in data:
+            image = cv.imread(d["imageLocation"], flags=cv.IMREAD_COLOR)
+            is_success, img_buf_arr = cv.imencode(".jpg", image)
+            del d["imageLocation"]
             d["editedAt"] = d["editedAt"].isoformat()
             d["eventStart"] = d["eventStart"].isoformat()
             d["eventEnd"] = d["eventEnd"].isoformat()
             d["distance"] = great_circle((longlat[1], longlat[0]), (d["location"][1], d["location"][0])).meters
+            d["imageByte"] = img_buf_arr.tobytes()
         return data
     except Exception as e:
         print(f"An error occurred while finding events: {e}")
